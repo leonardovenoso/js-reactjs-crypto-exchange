75cdb4955448332e80ff08d01a44eac5487e4bd4
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-13 00:24:07 +0800

    Set global font

--- pages/theme.js
+++ pages/theme.js
@@ -1,9 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Lato&family=Rubik&display=swap');
+
   html {
     box-sizing: border-box;
     font-size: 16px;
+  }
+
+  body {
+    font-family: 'Lato', sans-serif;
   }
 `;
 
