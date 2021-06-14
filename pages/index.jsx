1d7917ad2f88c57af2c328ae2e3a6596f10dc019
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-14 11:23:37 +0800

    [UI] Added select atom to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
+import fetchBankAccounts from '../services/accountService';
+
+import Select from './components/atoms/select';
+
 export default function CryptoExchange() {
+  const [bankAccounts, setBankAccounts] = useState([]);
+
+  useEffect(() => {
+    const start = async () => {
+      const res = await fetchBankAccounts();
+      const resJson = await res.json();
+      setBankAccounts(resJson);
+    };
+
+    start();
+  }, []);
+
   return (
     <div>
       <main>
-        <h1>Title</h1>
-        <h2>Subtitle</h2>
-        <p>This will be regular</p>
+        <h1>Crypto Exchange</h1>
+        <Select data-cy="bank-accounts">
+          {bankAccounts.map(account => (
+            <option
+              key={account.accountNumber}
+            >{`${account.bankName} - ${account.accountNumber} (${account.currency})`}</option>
+          ))}
+        </Select>
       </main>
     </div>
   );
