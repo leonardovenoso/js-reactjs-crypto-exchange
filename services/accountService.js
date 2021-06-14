ddf29637219309d9a5c7bd40093c774428735bed
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-14 17:52:12 +0800

    [UI] Added fetch transactions by account number

--- services/accountService.js
+++ services/accountService.js
@@ -10,4 +10,11 @@ const fetchBankAccounts = async () => {
   return Promise.resolve({ json: () => Promise.resolve(bankAccounts) });
 };
 
+export const fetchTransactionsByAccountNumber = async accountNumber => {
+  const account = accountInformation.find(
+    account => account.accountNumber === accountNumber
+  );
+  return Promise.resolve({ json: () => Promise.resolve(account.transactions) });
+};
+
 export default fetchBankAccounts;
