59781f5089471790c215babf44085754f6dde767
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-14 13:08:39 +0800

    [UT] Fetch transactions by account number

--- cypress/unitAndIntegration/unit/services/accountService.spec.js
+++ cypress/unitAndIntegration/unit/services/accountService.spec.js
@@ -1,16 +1,27 @@
 import fetchBankAccounts from '../../../../services/accountService';
+import { fetchTransactionsByAccountNumber } from '../../../../services/accountService';
 
 describe('Account Service', () => {
   let accountInformation;
 
   describe('fetchBankAccounts()', () => {
     before(async () => {
-      const result = await fetchBankAccounts();
-      accountInformation = await result.json();
+      const res = await fetchBankAccounts();
+      accountInformation = await res.json();
     });
 
     it('has 7 bank accounts', () => {
       expect(accountInformation.length).to.be.eql(7);
+    });
+  });
+
+  describe('fetchTransactionsByAccountNumber', () => {
+    describe('when account number is 13311110000', () => {
+      it('has 100 transactions', async () => {
+        const res = await fetchTransactionsByAccountNumber('13311110000');
+        const transactions = await res.json();
+        expect(transactions.length).to.be.eql(100);
+      });
     });
   });
 });
