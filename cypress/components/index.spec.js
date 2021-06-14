2ad8111a44129291ba71b0b6981905a58c4bae92
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-15 00:59:16 +0800

    [CT] Test for select and table behaviour

--- cypress/components/index.spec.js
+++ cypress/components/index.spec.js
@@ -17,10 +17,10 @@ describe('index', () => {
       cy.contains('Crypto Exchange');
     });
 
-    it('has a dropdown list with 7 bank accounts', () => {
+    it('has a dropdown list with 7 bank accounts plus select message', () => {
       cy.get('[data-cy=select-bank-account]')
         .children()
-        .should('have.length', 7);
+        .should('have.length', 8);
     });
   });
 
@@ -40,6 +40,28 @@ describe('index', () => {
       cy.get('[data-cy=transactions-header]').contains('Description');
       cy.get('[data-cy=transactions-header]').contains('Amount');
       cy.get('[data-cy=transactions-header]').contains('Currency');
+    });
+
+    it('renders a table body with 100 rows', () => {
+      cy.get('[data-cy=select-bank-account]').select('35625270301');
+      cy.get('[data-cy=transactions-body]')
+        .children()
+        .should('have.length', 100);
+    });
+  });
+
+  describe('when no bank account is selected', () => {
+    beforeEach(() => {
+      mount(
+        <ThemeProvider theme={darkTheme}>
+          <CryptoExchange />
+        </ThemeProvider>
+      );
+    });
+
+    it('suggests to select a bank account', () => {
+      cy.get('[data-cy=select-bank-account]').select('Select account');
+      cy.contains('Select a bank account');
     });
   });
 });
