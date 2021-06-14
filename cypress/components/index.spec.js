1a4a9ea9877b9e67a68837ec09ce93f8d36e3ff7
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-14 20:42:01 +0800

    [CT] Transaction table header

--- cypress/components/index.spec.js
+++ cypress/components/index.spec.js
@@ -1,16 +1,45 @@
+import { ThemeProvider } from 'styled-components';
 import { mount } from '@cypress/react';
+import { darkTheme } from '../../pages/theme';
 import CryptoExchange from '../../pages/index';
 
 describe('index', () => {
-  beforeEach(() => {
-    mount(<CryptoExchange />);
+  describe('page elements', () => {
+    beforeEach(() => {
+      mount(
+        <ThemeProvider theme={darkTheme}>
+          <CryptoExchange />
+        </ThemeProvider>
+      );
+    });
+
+    it('has a title', () => {
+      cy.contains('Crypto Exchange');
+    });
+
+    it('has a dropdown list with 7 bank accounts', () => {
+      cy.get('[data-cy=select-bank-account]')
+        .children()
+        .should('have.length', 7);
+    });
   });
 
-  it('has a title', () => {
-    cy.contains('Crypto Exchange');
-  });
+  describe('when a bank account is selected', () => {
+    beforeEach(() => {
+      mount(
+        <ThemeProvider theme={darkTheme}>
+          <CryptoExchange />
+        </ThemeProvider>
+      );
+    });
 
-  it('has a dropdown list with 7 bank accounts', () => {
-    cy.get('[data-cy=bank-accounts]').children().should('have.length', 7);
+    it('renders a table header', () => {
+      cy.get('[data-cy=select-bank-account]').select('13311110000');
+      cy.get('[data-cy=transactions-header]').contains('Time');
+      cy.get('[data-cy=transactions-header]').contains('Action');
+      cy.get('[data-cy=transactions-header]').contains('Description');
+      cy.get('[data-cy=transactions-header]').contains('Amount');
+      cy.get('[data-cy=transactions-header]').contains('Currency');
+    });
   });
 });
