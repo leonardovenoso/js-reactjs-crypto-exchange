8dda3d63e475e1ad30120b3850885d29a5adb628
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-16 00:47:33 +0800

    [UI] Gridtable molecule

--- pages/components/molecules/gridTable.js
+++ pages/components/molecules/gridTable.js
@@ -1,3 +1,31 @@
-const GridTable = () => {};
+import Table, { Header, Body, RowWithBorderBottom } from '../atoms/table';
+
+const GridTable = ({
+  columns,
+  columnLabels,
+  dataCyHeader,
+  dataCyBody,
+  list,
+  listObjAttrs,
+}) => (
+  <Table>
+    <Header data-cy={dataCyHeader} columns={`${'0.1fr'} ${columns}`}>
+      <div>#</div>
+      {columnLabels.map((label, i) => (
+        <div key={i}>{label}</div>
+      ))}
+    </Header>
+    <Body data-cy={dataCyBody}>
+      {list.map((el, i) => (
+        <RowWithBorderBottom key={i} columns={`${'0.1fr'} ${columns}`}>
+          <div>{i + 1}</div>
+          {listObjAttrs.map(attr => (
+            <div>{el[attr]}</div>
+          ))}
+        </RowWithBorderBottom>
+      ))}
+    </Body>
+  </Table>
+);
 
 export default GridTable;
