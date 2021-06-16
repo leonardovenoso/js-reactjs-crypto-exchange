1a9c8c59ed23f1e299c2a05969b950076c90615f
Leonardo A Venoso <quicksort.com@gmail.com>
2021-06-16 16:08:18 +0800

    [UI] Border radius

--- pages/components/atoms/button.js
+++ pages/components/atoms/button.js
@@ -5,7 +5,7 @@ const Button = styled.button`
   cursor: pointer;
   background: ${props => props.theme.colors.secondary};
   border: 1px solid ${props => props.theme.colors.black};
-  border-radius: 0 0.3em;
+  border-radius: ${props => props.theme.border.radious};
 
   &:hover {
     font-weight: bold;
--- pages/components/atoms/select.js
+++ pages/components/atoms/select.js
@@ -4,7 +4,7 @@ const Select = styled.select`
   height: 3em;
   color: ${props => props.theme.colors.darkgray};
   cursor: pointer;
-  border-radius: 0 0.3em;
+  border-radius: ${props => props.theme.border.radious};
   outline: none;
 `;
 
